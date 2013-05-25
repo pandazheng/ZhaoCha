137f48fc85d8b92b1e13090c18cd95892eb45c05
panda zheng <pandazheng@pandas-MacBook-Pro.local>
2013-05-24 17:23:23 -0700

    Initial Commit

---
+++ ZhaoCha/AppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.h
+//  ZhaoCha
+//
+//  Created by panda zheng on 13-5-24.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+@interface AppController : NSObject <UIApplicationDelegate, CCDirectorDelegate>
+{
+	UIWindow *window_;
+	UINavigationController *navController_;
+
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) UINavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
---
+++ ZhaoCha/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  ZhaoCha
+//
+//  Created by panda zheng on 13-5-24.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
